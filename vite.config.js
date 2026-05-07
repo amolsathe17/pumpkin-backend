1355119e6138752efba028e28a52868ad49193c8
unknown <Amols@Amol.comsoft.co.in>
2026-05-07 13:11:19 +0530

    first backend upload

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
+
+
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [react()],
+  
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      "brilliant-intuition-production-07c6.up.railway.app",
+      "pumpkinpicturesllp.uk",
+      "www.pumpkinpicturesllp.uk"
+    ]
+  }
+});
