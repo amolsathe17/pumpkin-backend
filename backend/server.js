599c4aa58802cb103389c101b8d49fa6f7b6d7d3
unknown <Amols@Amol.comsoft.co.in>
2026-05-07 10:56:53 +0530

    first backend upload

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
